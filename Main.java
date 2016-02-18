5b9abb48092ea51302536c69cd0e5442cfc80436
rmullally <rmullally@stu.jsu.edu>
2016-02-18 14:37:41 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // TO DO
+        // The program should roll 5 dice that are randomly chosen
+        // from the set [d4, d6, d8, d10, d12, d20, d100]. For
+        // instance, your program may randomly choose to roll
+        // 1d4 + 2d6 + 1d20 + 1d100
+        // If so, the output should show the rolls:
+        // d4 = 3
+        // d6 = 2
+        // d6 = 4
+        // d20 = 17
+        // d100 = 42
+        
+        int[] dieType = {4, 6, 8, 10, 12, 20, 100};
+        // Die d = new Die(dieType[0]);  // This would give us a d4.
+    }
+}
